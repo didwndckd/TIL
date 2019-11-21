2d14fc2f750a9138510f0f8653703beaf1f1e398
JoongChangYang <didwndckd@gmail.com>
2019-11-21 22:07:16 +0900

    TIL

---
+++ 과제/191121/191121/191121/SecondView.swift
@@ -0,0 +1,30 @@
+//
+//  SecondView.swift
+//  191121
+//
+//  Created by 양중창 on 2019/11/21.
+//  Copyright © 2019 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class SecondView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
