28b2dbfffbbc36bbcb229c7efbc6510f6717b30b
JoongChangYang <didwndckd@gmail.com>
2019-11-22 00:07:33 +0900

    TIL

--- 과제/191121/191121/191121/SecondView.swift
+++ 과제/191121/191121/191121/SecondView.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
+
+
 class SecondView: UIViewController {
 
+    let addButton = UIButton(type: .system)
+    let subButton = UIButton(type: .system)
+    let label = UILabel()
+    var num: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        label.frame = CGRect(x: 198.5, y: 220, width: 80, height: 80)
+        label.text = "0"
+        label.font = label.font.withSize(30)
+        view.addSubview(label)
+        
+        addButton.frame = CGRect(x: 90, y: 544, width: 80, height: 80)
+        addButton.setTitle("add", for: .normal)
+        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        view.addSubview(addButton)
+        addButton.addTarget(self, action: #selector(add), for: .touchUpInside)
+        
+        subButton.frame = CGRect(x: 274, y: 544, width: 80, height: 80)
+        subButton.setTitle("sub", for: .normal)
+        subButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        view.addSubview(subButton)
+        subButton.addTarget(self, action: #selector(sub), for: .touchUpInside)
+        //274 544
+        
+        
+        
     }
+    
+    
+    @objc func add () {
+        num += 1
+        label.text = String(num)
+        label.textColor = .blue
+    }
+    
+    @objc func sub() {
+        num -= 1
+        label.text = String(num)
+        label.textColor = .red
+    }
+    
+    
     
 
     /*
