0d67dcb95d591541a89d4281f97ff0ef1d1781df
JoongChangYang <didwndckd@gmail.com>
2019-12-04 16:41:11 +0900

    Start MyMemo project

---
+++ TIL_project/MyMemo/MyMemo/NaviView.swift
@@ -0,0 +1,40 @@
+//
+//  NaviView.swift
+//  MyMemo
+//
+//  Created by 양중창 on 2019/12/03.
+//  Copyright © 2019 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class NaviView: UIView {
+    
+    
+    
+     let editDirectory: UIButton = UIButton(type: .system)
+     let labelDirectory: UILabel = UILabel()
+     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init Error")
+    }
+    
+    
+    func setHeader(superView: UIView) {
+        
+    }
+    
+    func setUI() {
+        
+    }
+    
+}
+
+
---
+++ TIL_project/MyMemo/MyMemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyMemo
+//
+//  Created by 양중창 on 2019/12/03.
+//  Copyright © 2019 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let naviView = NaviView()
+        
+    }
+
+
+}
+
