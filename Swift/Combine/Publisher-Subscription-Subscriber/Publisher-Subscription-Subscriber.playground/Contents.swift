78b07c164064c1c7604e3e5d7c98782013fd94e9
didwndckd <didwndckd@gmail.com>
2022-12-10 03:14:18 +0900

    feat: 작업중 커밋

--- Swift/Combine/Publisher-Subscription-Subscriber/Publisher-Subscription-Subscriber.playground/Contents.swift
+++ Swift/Combine/Publisher-Subscription-Subscriber/Publisher-Subscription-Subscriber.playground/Contents.swift
@@ -10,9 +10,9 @@ struct CustomJust<Output, Failure: Error>: Publisher {
 
     func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
         Swift.print("CustomJust 구독 시작 -> \(type(of: subscriber))")
-//        let inner = Inner(subscriber: subscriber, data: self.data)
+//        let inner = CustomJustSubscription(subscriber: subscriber, data: self.data)
 //        subscriber.receive(subscription: inner)
-        
+//
         subscriber.receive(data)
         subscriber.receive(data)
         subscriber.receive(completion: .finished)
@@ -20,24 +20,24 @@ struct CustomJust<Output, Failure: Error>: Publisher {
 }
 
 extension CustomJust {
-    private final class Inner<S: Subscriber>: Subscription where S.Input == CustomJust.Output, S.Failure == CustomJust.Failure {
+    private final class CustomJustSubscription<S: Subscriber>: Subscription where S.Input == CustomJust.Output, S.Failure == CustomJust.Failure {
         
         private let data: CustomJust.Output
         private let subscriber: S
         private var demand: Subscribers.Demand = .none
         
         deinit {
-            Swift.print("Inner deinit")
+            Swift.print("CustomJustSubscription deinit")
         }
         
         init(subscriber: S, data: CustomJust.Output) {
-            Swift.print("Inner init")
+            Swift.print("CustomJustSubscription init")
             self.subscriber = subscriber
             self.data = data
         }
         
         func request(_ demand: Subscribers.Demand) {
-            Swift.print("Inner 데이터 요청: \(demand)")
+            Swift.print("CustomJustSubscription 데이터 요청: \(demand)")
             self.demand = demand
             
             guard self.demand > .none else {
@@ -47,7 +47,7 @@ extension CustomJust {
              
             self.demand = self.subscriber.receive(data)
             
-            Swift.print("Inner receive 이후 \(self.demand)")
+            Swift.print("CustomJustSubscription receive 이후 \(self.demand)")
             
             if self.demand == .none {
                 self.subscriber.receive(completion: .finished)
@@ -55,7 +55,7 @@ extension CustomJust {
         }
         
         func cancel() {
-            Swift.print("Inner cencel")
+            Swift.print("CustomJustSubscription cencel")
             self.subscriber.receive(completion: .finished)
         }
     }
@@ -94,6 +94,7 @@ let p = CustomJust<String, Never>("abc")
 
 print("===================CustomJustSubscriber===================")
 let s = CustomJustSubscriber<String, Never>()
+
 p.subscribe(s)
 
 print("===================Sink=====================")
@@ -119,6 +120,4 @@ cancelable.cancel()
 
 cancelable.store(in: &store)
 
-
-
-
+print("===================Just, CustomJustSubscriber=====================")
