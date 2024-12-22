631b9ba1b7dcff36341d99a16f5b94ffa8469f16
didwndckd <didwndckd@gmail.com>
2024-12-22 23:44:57 +0900

    Sendable 준비

---
+++ Swift/Concurrency/Sendable/Sendable.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Combine
+
+//extension Publisher where Failure == Never {
+//    func asAsync() async -> Output {
+//        await withCheckedContinuation { continuation in
+//            let _ = self.sink { continuation.resume(returning: $0) }
+//        }
+//    }
+//}
